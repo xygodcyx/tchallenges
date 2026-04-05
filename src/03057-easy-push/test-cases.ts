32f7d557f345e5be098be5bc831350a5dc3fffad
xygodcyx <1323943635@qq.com>
2026-04-05 12:15:22 +0800

    add easy-push

---
+++ src/03057-easy-push/test-cases.ts
@@ -0,0 +1,14 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Expect<Equal<Push<number[], string>, string[]>>,
+  // @ts-expect-error
+  Expect<Equal<Push<string[], number>, [string, number]>>,
+]
